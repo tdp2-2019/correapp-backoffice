1d11aec7d482359d6286f3506ccd54aecab0c229
mcerrotta <matias.cerrotta@gmail.com>
2019-06-02 19:47:57 -0300

    Agrego maquetación faltante a Ficha de viaje

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Choferes = React.lazy(() => import('./views/Base/Choferes/Choferes'));
 const Chofer = React.lazy(() => import('./views/Base/Choferes/Chofer'));
 const Viajes = React.lazy(() => import('./views/Base/Viajes/Viajes'));
-const Viaje = React.lazy(() => import('./views/Base/Viajes/Mapa'));
+const Viaje = React.lazy(() => import('./views/Base/Viajes/Viaje'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
